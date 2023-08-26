26afa4955e53e6fe9d6e5d1c3fbab55c8ffd16f9
Amrit Prusty <amrit2995@gmail.com>
2023-08-26 16:01:05 +0530

    initial commits

--- blog/blog_project/settings.py
+++ blog/blog_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig', #new
+    'accounts.app.AccountCofig',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
